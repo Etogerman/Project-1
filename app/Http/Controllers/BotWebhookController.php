06b413aff6164baf14a53d4ee18c2d4f8a8782b0
German Abrikosov <etogerman@gmail.com>
2026-04-11 21:54:49 +0300

    Открыть generic callback gate для DB-сценариев

--- app/Http/Controllers/BotWebhookController.php
+++ app/Http/Controllers/BotWebhookController.php
@@ -11,7 +11,6 @@ use App\Services\Bots\ChannelActivityLogger;
 use App\Services\Bots\DispatchStoredInboundBotMessageAction;
 use App\Services\Bots\StoreInboundMessageAction;
 use App\Services\Bots\TelegramBotApiService;
-use App\Services\Scenarios\ScenarioRegistry;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -433,7 +432,7 @@ class BotWebhookController extends Controller
             return false;
         }
 
-        return app(ScenarioRegistry::class)->type($run->scenario_code) === 'builtin';
+        return true;
     }
 
     /**
