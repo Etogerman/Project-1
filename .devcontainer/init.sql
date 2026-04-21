7e32aeed6ef8dcc2e95182c3b02e548d13310bbb
Ajay Verma <ajayver.com@gmail.com>
2026-04-21 15:02:44 +0000

    Docker

---
+++ .devcontainer/init.sql
@@ -0,0 +1,5 @@
+-- Create testing database for Laravel tests
+CREATE DATABASE laravel_testing;
+
+-- Grant privileges to the app user on the testing database
+GRANT ALL PRIVILEGES ON DATABASE laravel_testing TO abrikosoff;
