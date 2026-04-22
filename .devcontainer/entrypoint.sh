c670a276b00021def82d09c470c2fc103bbb828b
Ajay Verma <ajayver.com@gmail.com>
2026-04-22 15:38:12 +0000

    Works

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Trust the workspace repo
+git config --global --add safe.directory /var/www/html
+
 # Start Laravel dev server if the app is ready
 if [ -f /var/www/html/artisan ] && [ -d /var/www/html/vendor ]; then
     cd /var/www/html
