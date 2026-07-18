93e4fc8b34918c62a05b94974f0366ff5e57a07c
German Abrikosov <etogerman@gmail.com>
2026-07-18 18:11:29 +0300

    Усилить защиту Telegram file bridge

--- .devcontainer/telegram-bot-api-file-bridge/40-create-basic-auth.sh
+++ .devcontainer/telegram-bot-api-file-bridge/40-create-basic-auth.sh
@@ -10,5 +10,5 @@ printf '%s\n' "${TELEGRAM_LOCAL_BOT_API_FILE_BRIDGE_PASSWORD}" \
         /etc/nginx/.telegram-bot-api-files.htpasswd \
         "${TELEGRAM_LOCAL_BOT_API_FILE_BRIDGE_USERNAME}"
 
-chown root:nginx /etc/nginx/.telegram-bot-api-files.htpasswd
+chown root:telegram-media /etc/nginx/.telegram-bot-api-files.htpasswd
 chmod 640 /etc/nginx/.telegram-bot-api-files.htpasswd
